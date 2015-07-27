bda6e8f60a5d82726a0570c137209160b841eee9
Alexander Egorenkov <aegorenk@gmail.com>
2015-07-27 13:01:50 -0400

    Add script for scraping 200 most recent statuses for each user

---
+++ scrape_user_tweets.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 02:28:01 2015
+
+@author: Alexander
+"""
+
+from os import chdir
+from os import listdir
+from pandas import read_csv
+import twitter
+import pandas as pd
+import itertools
+
+directory = r'C:\Users\Alexander\Documents\Programming\DAT7\DAT7project'
+chdir(directory)
+
+class UserTimelineCache(object):
+    '''
+    Maintains a rudimentary cache 
+    to avoid unnecessarily querying the twitter API
+    '''
+    
+    def __init__(self, cache_directory=directory + r'/user_timelines/'):
+        self.cache_directory = cache_directory
+        self.load_cache()
+    
+    def load_cache(self):
+        '''Extracts user ids from cache directory'''
+        files = listdir(self.cache_directory)
+        cached_users = (f.split('_')[0] for f in files)
+        user_lookup = dict()
+        for user_id in cached_users:
+            user_key = str(user_id)
+            user_lookup[user_key] = True
+        self._users = user_lookup
+    
+    def has_user(self, user_id):
+        '''Determines whether a user id is in the cache'''
+        user_key = str(user_id)
+        return self._users.get(user_key, False)
+
+class UserTimeline(object):
+    
+    def __init__(self, user_id, cache):
+        self.user_id = str(user_id)
+        self._cache = cache
+        
+    def user_cached(self):
+        self._cache.load_cache()
+        return self._cache.has_user(self.user_id)
+        
+    def load(self):
+        if not self.user_cached():
+            try:
+                user_timeline = api.GetUserTimeline(user_id=self.user_id, count=200)
+                print user_timeline
+                #Make this a dictionary with json values and status key
+                keys = [status.GetId() for status in user_timeline]
+                values = [u.AsJsonString() for u in user_timeline]
+                self._json = dict(zip(keys, values))
+                return True
+            except twitter.TwitterError:
+                self._json = '{"message": "This id is not associated with a user"}'
+                return False
+        
+    def save(self):
+        if not self.user_cached():
+            for key in self._json.iterkeys():
+                file_name = self._cache.cache_directory + self.user_id + '_' + str(key) +  '.json'      
+                with open(file_name, 'w') as json_file:
+                    json_file.write(self._json[key])
+                    json_file.close()
+            return True
+
+with open('keys/consumer_key.txt','r') as f:
+    consumer_key = f.read()
+    
+with open('keys/consumer_secret.txt','r') as f:
+    consumer_secret = f.read()
+    
+with open('keys/access_token_key.txt','r') as f:
+    access_token_key = f.read()
+    
+with open('keys/access_token_secret.txt','r') as f:
+    access_token_secret = f.read()
+
+api = twitter.Api(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token_key=access_token_key,
+    access_token_secret=access_token_secret,
+)
+
+def scrape_user_tweets(rate, timer):
+    if timer.ready():
+        user_timeline_cache = UserTimelineCache(directory + r'/user_timelines/')
+        #Get user list
+        personal_victory = pd.read_csv(r'data\personalvictory.csv')
+        random_user = pd.read_csv(r'data\random_user.csv')
+        retweet_list = pd.read_csv(r'data\retweeter_list.csv')
+        users_pv = [s for s in personal_victory.user_id if not user_timeline_cache.has_user(s)]
+        users_ru = [s for s in random_user.id if not user_timeline_cache.has_user(s)]
+        users_rl = [s for s in retweet_list.user_id if not user_timeline_cache.has_user(s)]
+
+        user_list = list(itertools.chain(users_pv, users_ru, users_rl))
+
+        if user_list:
+            user = UserTimeline(user_list[0], user_timeline_cache)
+            user.load()
+            user.save()
+        timer.set_next_run(rate)
+    else:
+        pass 
--- scraper.py
+++ scraper.py
@@ -14,6 +14,7 @@ from scrape_random_users import scrape_random_user
 from scrape_retweeters import scrape_retweeter
 from scrape_retweeting_users import scrape_retweeting_user
 from load_retweet_list import load_retweet_list
+from scrape_user_tweets import scrape_user_tweets
 from timing import ScrapeTimer
 
 directory = r'C:\Users\Alexander\Documents\Programming\DAT7\DAT7project'
@@ -23,16 +24,17 @@ chdir(directory)
 retweeter_timer = ScrapeTimer()
 ruser_timer = ScrapeTimer()
 retuser_timer = ScrapeTimer()
+user_tweet_timer = ScrapeTimer()
 
 #15 minute epoch
 sec5_in_min15 = 30*60/5
 for sec5 in xrange(sec5_in_min15):
     sleep(5)
-    scrape_retweeter(63, retweeter_timer)
-    load_retweet_list(retweeter_timer) #required update for scraping retweeting users
+    #scrape_retweeter(63, retweeter_timer)
+    #load_retweet_list(retweeter_timer) #required update for scraping retweeting users
     
     scrape_random_user(12, ruser_timer)
     scrape_retweeting_user(10,retuser_timer)
+    scrape_user_tweets(6, user_tweet_timer)
     #scrape_poster_user
-    #scrape_user_tweets
     
