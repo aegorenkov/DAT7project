77cd876995d702a9c8908ec16052305a97a31f31
Alexander Egorenkov <aegorenk@gmail.com>
2015-07-30 17:45:49 -0400

    Refactor location of loads for sanity

---
+++ loaders/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['data',
+           'personalvictories',
+           'random_users',
+           'retweet_list',
+           'user_timelines',
+           'users']
